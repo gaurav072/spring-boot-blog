287736266decd9772ea4ebc558365e0ae43a5ea2
Gaurav Kumar <gauravsrivastav04@gmail.com>
2018-03-05 08:01:06 +0530

    Changes added

--- src/main/java/com/startwithjava/simpleblog/rest/swagger/SwaggerConfig.java
+++ src/main/java/com/startwithjava/simpleblog/rest/swagger/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.startwithjava.simpleblog.controllers"))
                 .paths(regex("/product.*"))
                 .build()
                 .apiInfo(metaData());
